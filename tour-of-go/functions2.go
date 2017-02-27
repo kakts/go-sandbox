991ee0779742b9add82972cbe6ad18ce6c39bdec
kakts <akutsukeita@gmail.com>
2017-02-27 22:39:16 +0900

    Initial tour-of-go

---
+++ tour-of-go/functions2.go
@@ -0,0 +1,9 @@
+package testfun
+
+import (
+  "fmt"
+)
+
+func Add(x int, y int) int {
+  return x + y
+}
---
+++ tour-of-go/functions3.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+  "fmt"
+  "./functions2"
+)
+
+func add(x int, y int) int {
+  return x + y
+}
+
+func add2(x, y int) {
+  return x + y
+}
+
+func main() {
+  Add(1, 2)
+}

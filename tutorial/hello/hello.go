297f95b2ccaab5bc74cc1c6f7f88b3e91e223d1c
kakts <kakts.git@gmail.com>
2020-11-24 00:58:57 +0900

    add: greetings.Hellos

--- tutorial/hello/hello.go
+++ tutorial/hello/hello.go
@@ -13,13 +13,16 @@ func main() {
 	log.SetPrefix("greetings: ")
 	log.SetFlags(0)
 
+	// A slice of names.
+	names := [] string{"Gladys", "Samantha", "Darrin"}
+
 	// Get a greeting message and print it.
-	message, err := greetings.Hello("John")
+	messages, err := greetings.Hellos(names)
 	// If an error was returned, print it to the console and exit the program
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	// If no error was returned, print the returned message to the console.
-	fmt.Println(message)
+	fmt.Println(messages)
 }

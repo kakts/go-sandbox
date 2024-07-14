e7ff5977a4541a83b14c9b5e73d253a70765dd28
kakts <kakts.git@gmail.com>
2024-07-15 00:48:27 +0900

    [goroutine] feat: add WaitGroupMulti

--- goroutine/src/goroutine/waitgroup.go
+++ goroutine/src/goroutine/waitgroup.go
@@ -27,3 +27,20 @@ func WaitGroup() {
 	wg.Wait()
 	fmt.Println("All goroutines are finished")
 }
+
+func WaigGroupMulti() {
+	hello := func(wg *sync.WaitGroup, id int) {
+		defer wg.Done()
+		fmt.Printf("Hello from %v!\n", id)
+	}
+
+	const numGreeters = 5
+	var wg sync.WaitGroup
+
+	// goroutine呼び出し前に1度で全てのgoroutineの数を指定する
+	wg.Add(numGreeters)
+	for i := 0; i < numGreeters; i++ {
+		go hello(&wg, i+1)
+	}
+	wg.Wait()
+}
--- goroutine/src/main.go
+++ goroutine/src/main.go
@@ -3,5 +3,7 @@ package main
 import "github.com/kakts/go-sandbox/goroutine/src/goroutine"
 
 func main() {
-	goroutine.WaitGroup()
+	// goroutine.WaitGroup()
+
+	goroutine.WaigGroupMulti()
 }

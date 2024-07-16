afd252de50ef267361665b6d0bdc84337c262ba5
kakts <kakts.git@gmail.com>
2024-07-17 00:27:03 +0900

    [goroutine] feat: add BufferedChannel

--- goroutine/src/goroutine/channel.go
+++ goroutine/src/goroutine/channel.go
@@ -1,6 +1,11 @@
 package goroutine
 
-import "fmt"
+import (
+	"bytes"
+	"fmt"
+	"os"
+	"sync"
+)
 
 func SimpleChannel() {
 	stringStream := make(chan string)
@@ -37,5 +42,47 @@ func ChannelLoop() {
 	// チャネルに対するrangeループは、チャネルが閉じられるまでデータを受信し続ける
 	for integer := range intStream {
 		fmt.Printf("%v ", integer)
+	}
+}
+
+func ReleaseChannelForMultiGoroutine() {
+	begin := make(chan interface{})
+	var wg sync.WaitGroup
+
+	for i := 0; i < 4; i++ {
+		wg.Add(1)
+		go func(i int) {
+			defer wg.Done()
+			// チャネルからの読見込めるようになるまでブロック
+			<-begin
+			fmt.Printf("%v has begun\n", i)
+		}(i)
+	}
+
+	fmt.Println("Unblocking goroutines...")
+	// チャネルを閉じる　これにより全てのgoroutineが同時に開始される
+	close(begin)
+	wg.Wait()
+}
+
+func BufferedChannel() {
+	var stdoutBuff bytes.Buffer
+	defer stdoutBuff.WriteTo(os.Stdout)
+
+	// バッファ付きチャネル 4つまではブロッキングなしでデータを送受信できる
+	intStream := make(chan int, 4)
+	go func() {
+		defer close(intStream)
+		defer fmt.Fprintln(&stdoutBuff, "Producer Done.")
+
+		for i := 0; i < 5; i++ {
+			fmt.Fprintf(&stdoutBuff, "SEnding: %d\n", i)
+			// バッファ付きチャネルへの送信 4つまではブロッキングなし
+			intStream <- i
+		}
+	}()
+
+	for integer := range intStream {
+		fmt.Fprintf(&stdoutBuff, "Received: %v\n", integer)
 	}
 }

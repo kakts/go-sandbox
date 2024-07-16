7260c22b9c7caa0bc1c711834011b3c858ef9ebe
kakts <kakts.git@gmail.com>
2024-07-17 00:12:50 +0900

    [goroutine] feat: add ChannelLoop

--- goroutine/src/goroutine/channel.go
+++ goroutine/src/goroutine/channel.go
@@ -9,5 +9,33 @@ func SimpleChannel() {
 		stringStream <- "Hello channels!"
 	}()
 
+	// チャネルからデータを受信するまでブロック
 	fmt.Println(<-stringStream)
+}
+
+func ChannelReceiveCheck() {
+	stringStream := make(chan string)
+	go func() {
+		stringStream <- "Hello channels!"
+	}()
+
+	// チャネルの第2戻り値はチャネルが受信成功したかをチェックする
+	// falseの場合 チャネルが閉じられていてこれ以上受信できないことを示す
+	salutation, ok := <-stringStream
+	fmt.Printf("(%v): %v\n", ok, salutation)
+}
+
+func ChannelLoop() {
+	intStream := make(chan int)
+	go func() {
+		defer close(intStream)
+		for i := 1; i <= 5; i++ {
+			intStream <- i
+		}
+	}()
+
+	// チャネルに対するrangeループは、チャネルが閉じられるまでデータを受信し続ける
+	for integer := range intStream {
+		fmt.Printf("%v ", integer)
+	}
 }
